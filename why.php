705568ecfc973bda36d9f6ba4b7064b018081ad2
grindfather <memphis@ip0af57a85.int.uvm.edu>
2016-12-06 18:46:04 -0500

    change
    change

--- why.php
+++ why.php
@@ -8,6 +8,14 @@
         </div>
         <div class="container" style="margin-top: 80px;">
             <p>This page tells users why are we making this website.</p>
+            <p> Our availability </p>
+            <iframe src="https://calendar.google.com/calendar/embed?src=bovhhkrf8vlnnn8tf9kur8f9io%40group.calendar.google.com&ctz=America/New_York" 
+                    style="border: 0" 
+                    width="800" 
+                    height="600" 
+                    frameborder="0" 
+                    scrolling="no">     
+            </iframe>
         </div>
         <?php include "footer.php"; ?>
     </body>
