818016f7d311011fb9e6d8c09b58abdfce00ba2f
grindfather <memphis@ip0af57a85.int.uvm.edu>
2016-12-06 18:31:15 -0500

    change
    change

--- head.php
+++ head.php
@@ -1,7 +1,7 @@
 <?php ?>
 
 <head>
-    <title>UVM Food Item Express</title>
+    <title> UVM Food Item Express</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
--- index.php
+++ index.php
@@ -7,7 +7,42 @@
             <?php include "nav.php"; ?>
         </div>
         <div class="container" style="margin-top: 80px;">
-            <p>This is the home page.</p>
+            <p>Welcome to Midnight Delivery!  Ever had an experience in University of Vermont when you are studying till midnight and you are so hungry but you don’t want to swallow your pride to go to Grundle for those terrible meals?</p> 
+            <p>Of course, you do!</p>
+            <p>But what did you do next?</p>
+            <ul>
+                <li>Go around your hall and beg your friends for more food!? LOL</li>
+                <li>Order Pizza takeout!? (Calories…mmm)</li>
+                <li>Order Wings takeout!? (Calories…mmm)</li>
+                <li>Call your parents and cry!?</li>
+                <li>Tell yourself “Hunger is for the weak” and then go sleep it off (what about the assignment that’s due tomorrow??)</li>
+            </ul>
+            <p>Well, thank god for Midnight Delivery and now you have an option. </p>
+            <p>Just hit us with the <s>DM</s> MD!!</p>
+        <div>
+               <p>MD offers the following:</p>
+               <ul>
+                   <li>door-to-door delivery at flexible time</li>
+                   <li>variety of food options, ranging from crackers to a bowl of pasta</li>
+                   <li>credit/debit payment or cash </li>
+                   <li>we provide price comparison of our items with on-campus diner</li>
+                   <li>strives to import food options from out of states (snacks/simple meal from foreign countries)</li>
+                   <li><strong>DO</strong> not accept tips**</li>
+               </ul>
+
+        </div>
+        <div>
+            <p>**</p>
+            <p>“Would you like a smile with your General Tsao chicken with rice? That would be extra 10%. Thanks ”</p>
+            <p>-->MD strongly rejects the idea of tipping and we are promoting a non-tipping culture due to the following reasons:</p>
+            <ol>
+                <li>Strong correlation with tipping and sexual harassment</li>
+                <li>Tipping system tips the scale of power heavily towards the customers and we believe that customers and staffs should be both held at an equal balance of power. </li>
+                <li>Tipping creates an extra layer of superficiality within our society. </li>
+                <li>Staffs should not only serve well with the main purpose of earning more tips but with genuine hospitality that comes from the bottom of their hearts. </li>
+                
+            </ol>
+        </div>
         </div>
         <?php include "footer.php"; ?>
     </body>
