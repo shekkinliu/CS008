b97a0799210b8a2b395064f8a65007fddc05b6f4
Ken Liu <shekkinliu@gmail.com>
2016-12-07 08:34:26 -0500

    Every page has their own title :)

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php ?>
 <!DOCTYPE html>
 <html lang="en">
-    <?php include "head.php"; ?>
+    <head>
+        <title>Home Page - Midnight Delivery</title>
+        <?php include "head.php"; ?>
+    </head>
     <body>
         <div>
             <?php include "nav.php"; ?>
@@ -20,28 +23,28 @@
             <p>Well, thank god for Midnight Delivery and now you have an option. </p>
             <p>Just hit us with the <acronym title="Direct Message"><s>DM</s></acronym> MD!!</p>
         <div>
-               <p>MD offers the following:</p>
-               <ul>
-                   <li>door-to-door delivery at flexible time</li>
-                   <li>variety of food options, ranging from crackers to a bowl of pasta</li>
-                   <li>credit/debit payment or cash </li>
-                   <li>we provide price comparison of our items with on-campus diner</li>
-                   <li>strives to import food options from out of states (snacks/simple meal from foreign countries)</li>
-                   <li><strong>DO</strong> not accept tips<a href="#tipping">**</a></li>
-               </ul>
+            <p>MD offers the following:</p>
+            <ul>
+                <li>door-to-door delivery at flexible time</li>
+                <li>variety of food options, ranging from crackers to a bowl of pasta</li>
+                <li>credit/debit payment or cash </li>
+                <li>we provide price comparison of our items with on-campus diner</li>
+                <li>strives to import food options from out of states (snacks/simple meal from foreign countries)</li>
+                <li><strong>DO</strong> not accept tips<a href="#tipping">**</a></li>
+            </ul>
         </div>
-            
-            <p id="tipping">**</p>
-            <p>“Would you like a smile with your General Tsao chicken with rice? That would be extra 10%. Thanks ”</p>
-            <p>-->MD strongly rejects the idea of tipping and we are promoting a non-tipping culture due to the following reasons:</p>
-            <ol>
-                <li>Strong correlation with tipping and sexual harassment</li>
-                <li>Tipping system tips the scale of power heavily towards the customers and we believe that customers and staffs should be both held at an equal balance of power. </li>
-                <li>Tipping creates an extra layer of superficiality within our society. </li>
-                <li>Staffs should not only serve well with the main purpose of earning more tips but with genuine hospitality that comes from the bottom of their hearts. </li>
-                
-            </ol>
-        </div>
-        <?php include "footer.php"; ?>
-    </body>
+
+        <p id="tipping">**</p>
+        <p>“Would you like a smile with your General Tsao chicken with rice? That would be extra 10%. Thanks ”</p>
+        <p>-->MD strongly rejects the idea of tipping and we are promoting a non-tipping culture due to the following reasons:</p>
+        <ol>
+            <li>Strong correlation with tipping and sexual harassment</li>
+            <li>Tipping system tips the scale of power heavily towards the customers and we believe that customers and staffs should be both held at an equal balance of power. </li>
+            <li>Tipping creates an extra layer of superficiality within our society. </li>
+            <li>Staffs should not only serve well with the main purpose of earning more tips but with genuine hospitality that comes from the bottom of their hearts. </li>
+
+        </ol>
+    </div>
+    <?php include "footer.php"; ?>
+</body>
 </html>
--- shopping.php
+++ shopping.php
@@ -1,7 +1,10 @@
 <?php ?>
 <!DOCTYPE html>
 <html lang="en">
-    <?php include "head.php"; ?>
+    <head>
+        <title>Shopping - Midnight Delivery</title>
+        <?php include "head.php"; ?>
+    </head>
     <body>
         <div>
             <?php include "nav.php"; ?>
