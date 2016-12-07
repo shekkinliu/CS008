9c070e6c402126b94c7526902abe7ff2b000d8d0
Ken Liu <shekkinliu@gmail.com>
2016-12-07 00:25:03 -0500

    don't worry bout it

--- join.php
+++ join.php
@@ -8,6 +8,18 @@
         </div>
         <div class="container" style="margin-top: 80px;">
             <p>Join us Today!!!</p>
+            <form action="action_page.php">
+                <fieldset>
+                    <legend>Personal information:</legend>
+                    First name:<br>
+                    <input type="text" name="firstname" value="Mickey">
+                    <br>
+                    Last name:<br>
+                    <input type="text" name="lastname" value="Mouse">
+                    <br><br>
+                    <input type="submit" value="Submit">
+                </fieldset>
+            </form>
         </div>
         <?php include "footer.php"; ?>
     </body>
