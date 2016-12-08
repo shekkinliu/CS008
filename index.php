91a6f429d33d50e85b34b4c62222d993ec137e63
grindfather <memphis@ip0af50900.int.uvm.edu>
2016-12-08 14:00:01 -0500

    changed index.php but its all messed up
    changed index.php but its all messed up

--- index.php
+++ index.php
@@ -10,79 +10,54 @@
             <?php include "nav.php"; ?>
         </div>
         <div class="container" style="margin-top: 80px;">
-            <p>Welcome to Midnight Delivery!  Ever had an experience in University of Vermont when you are studying till midnight and you are so hungry but you don’t want to swallow your pride to go to Grundle for those terrible meals?</p> 
-            <p>Of course, you do!</p>
-            <p>But what did you do next?</p>
-            <ul>
-                <li>Go around your hall and beg your friends for more food!? LOL</li>
-                <li>Order Pizza takeout!? (Calories…mmm)</li>
-                <li>Order Wings takeout!? (Calories…mmm)</li>
-                <li>Call your parents and cry!?</li>
-                <li>Tell yourself “Hunger is for the weak” and then go sleep it off (what about the assignment that’s due tomorrow??)</li>
-            </ul>
-            <p>Well, thank god for Midnight Delivery and now you have an option. </p>
-            <p>Just hit us with the <acronym title="Direct Message"><s>DM</s></acronym> MD!!</p>
-        <div>
+            <div id="topbox">
+                <p>Welcome to Midnight Delivery!  Ever had an experience in University of Vermont when you are studying till midnight and you are so hungry but you don’t want to swallow your pride to go to Grundle for those terrible meals?</p> 
+                <img id="hungry" src="image/hungry.jpg" alt="">
+            </div>
+            <div id="mid1box" class="left">
+                <p>Of course, you do!</p>
+                <p>But what did you do next?</p>
+                <ul>
+                    <li>Go around your hall and beg your friends for more food!? LOL</li>
+                    <li>Order Pizza takeout!? (Calories…mmm)</li>
+                    <li>Order Wings takeout!? (Calories…mmm)</li>
+                    <li>Call your parents and cry!?</li>
+                    <li>Tell yourself “Hunger is for the weak” and then go sleep it off (what about the assignment that’s due tomorrow??)</li>
+                </ul>
+            </div>
+            <img id="arrow"  class="left" src="image/arrow.png" alt="" >
+            <div id="mid2box" class="left">
+                <p>Well, thank god for Midnight Delivery and now you have an option. </p>
+                <p>Just hit us with the <acronym title="Direct Message"><s>DM</s></acronym> MD!!</p>
 
-            <p>MD offers the following:</p>
-            <ul>
-                <li>door-to-door delivery at flexible time</li>
-                <li>variety of food options, ranging from crackers to a bowl of pasta</li>
-                <li>credit/debit payment or cash </li>
-                <li>we provide price comparison of our items with on-campus diner</li>
-                <li>strives to import food options from out of states (snacks/simple meal from foreign countries)</li>
-                <li><strong>DO</strong> not accept tips<a href="#tipping">**</a></li>
-            </ul>
+
+                <p>MD offers the following:</p>
+                <ul>
+                    <li>door-to-door delivery at flexible time</li>
+                    <li>variety of food options, ranging from crackers to a bowl of pasta</li>
+                    <li>credit/debit payment or cash </li>
+                    <li>we provide price comparison of our items with on-campus diner</li>
+                    <li>strives to import food options from out of states (snacks/simple meal from foreign countries)</li>
+                    <li><strong>DO</strong> not accept tips<a href="#tipping">**</a></li>
+                </ul>
+            </div>
+            
+
+
+            <div id="bottombox">
+                <p id="tipping">**</p>
+                <p>“Would you like a smile with your General Tsao chicken with rice? That would be extra 10%. Thanks ”</p>
+                <p>-->MD strongly rejects the idea of tipping and we are promoting a non-tipping culture due to the following reasons:</p>
+                <ol>
+                    <li>Strong correlation with tipping and sexual harassment</li>
+                    <li>Tipping system tips the scale of power heavily towards the customers and we believe that customers and staffs should be both held at an equal balance of power. </li>
+                    <li>Tipping creates an extra layer of superficiality within our society. </li>
+                    <li>Staffs should not only serve well with the main purpose of earning more tips but with genuine hospitality that comes from the bottom of their hearts. </li>
+
+                </ol>
+
+            </div>
         </div>
-        <p>blablablablablablablablablablablabla</p>
-        <p>blablablablablablablablablablablabla</p>
-        <p>blablablablablablablablablablablabla</p>
-        <p>blablablablablablablablablablablabla</p>
-        <p>blablablablablablablablablablablabla</p>
-        <p>blablablablablablablablablablablabla</p>
-        <p>blablablablablablablablablablablabla</p>
-        <p>blablablablablablablablablablablabla</p>
-        <p>blablablablablablablablablablablabla</p>
-        <p>blablablablablablablablablablablabla</p>
-        <p>blablablablablablablablablablablabla</p>
-        <p>blablablablablablablablablablablabla</p>
-
-
-
-        <p id="tipping">**</p>
-        <p>“Would you like a smile with your General Tsao chicken with rice? That would be extra 10%. Thanks ”</p>
-        <p>-->MD strongly rejects the idea of tipping and we are promoting a non-tipping culture due to the following reasons:</p>
-        <ol>
-            <li>Strong correlation with tipping and sexual harassment</li>
-            <li>Tipping system tips the scale of power heavily towards the customers and we believe that customers and staffs should be both held at an equal balance of power. </li>
-            <li>Tipping creates an extra layer of superficiality within our society. </li>
-            <li>Staffs should not only serve well with the main purpose of earning more tips but with genuine hospitality that comes from the bottom of their hearts. </li>
-
-        </ol>
-
-        <p>MD offers the following:</p>
-        <ul>
-            <li>door-to-door delivery at flexible time</li>
-            <li>variety of food options, ranging from crackers to a bowl of pasta</li>
-            <li>credit/debit payment or cash </li>
-            <li>we provide price comparison of our items with on-campus diner</li>
-            <li>strives to import food options from out of states (snacks/simple meal from foreign countries)</li>
-            <li><strong>DO</strong> not accept tips<a href="#tipping">**</a></li>
-        </ul>
-
-
-
-        <p id="tipping">**</p>
-        <p>“Would you like a smile with your General Tsao chicken with rice? That would be extra 10%. Thanks ”</p>
-        <p>-->MD strongly rejects the idea of tipping and we are promoting a non-tipping culture due to the following reasons:</p>
-        <ol>
-            <li>Strong correlation with tipping and sexual harassment</li>
-            <li>Tipping system tips the scale of power heavily towards the customers and we believe that customers and staffs should be both held at an equal balance of power. </li>
-            <li>Tipping creates an extra layer of superficiality within our society. </li>
-            <li>Staffs should not only serve well with the main purpose of earning more tips but with genuine hospitality that comes from the bottom of their hearts. </li>
-
-        </ol>
-    </div>
-    <?php include "footer.php"; ?>
-</body>
+        <?php include "footer.php"; ?>
+    </body>
 </html>
