ab2ceb7896d26edfcc6f4665f34e9e9415b71273
Ken Liu <shekkinliu@gmail.com>
2016-12-09 10:22:18 -0500

    some changes

--- index.php
+++ index.php
@@ -10,27 +10,29 @@
             <?php include "nav.php"; ?>
         </div>
         <div class="container" style="margin-top: 80px;">
-            <div id="topbox">
-                <p>Welcome to Midnight Delivery!  Ever had an experience in University of Vermont when you are studying till midnight and you are so hungry but you don’t want to swallow your pride to go to Grundle for those terrible meals?</p> 
-                <img id="hungry" src="image/hungry.jpg" alt="">
+            <div>
+                <div>
+                    <p>Welcome to Midnight Delivery!  Ever had an experience in University of Vermont when you are studying till midnight and you are so hungry but you don’t want to swallow your pride to go to Grundle for those terrible meals?</p> 
+                    <img id="hungry" src="image/hungry.jpg" alt="">
+                </div>
+                <div>
+                    <p>Of course, you do!</p>
+                    <p>But what did you do next?</p>
+                    <ul>
+                        <li>Go around your hall and beg your friends for more food!? LOL</li>
+                        <li>Order Pizza takeout!? (Calories…mmm)</li>
+                        <li>Order Wings takeout!? (Calories…mmm)</li>
+                        <li>Call your parents and cry!?</li>
+                        <li>Tell yourself “Hunger is for the weak” and then go sleep it off (what about the assignment that’s due tomorrow??)</li>
+                    </ul>
+                </div>
             </div>
-            <div id="mid1box" class="left">
-                <p>Of course, you do!</p>
-                <p>But what did you do next?</p>
-                <ul>
-                    <li>Go around your hall and beg your friends for more food!? LOL</li>
-                    <li>Order Pizza takeout!? (Calories…mmm)</li>
-                    <li>Order Wings takeout!? (Calories…mmm)</li>
-                    <li>Call your parents and cry!?</li>
-                    <li>Tell yourself “Hunger is for the weak” and then go sleep it off (what about the assignment that’s due tomorrow??)</li>
-                </ul>
-            </div>
-            <img id="arrow"  class="left" src="image/arrow.png" alt="" >
-            <div id="mid2box" class="left">
+
+
+            <div id="arrowContent">
+                <img src="image/arrow.png" alt="An arrow finger" id="arrow">
                 <p>Well, thank god for Midnight Delivery and now you have an option. </p>
                 <p>Just hit us with the <acronym title="Direct Message"><s>DM</s></acronym> MD!!</p>
-
-
                 <p>MD offers the following:</p>
                 <ul>
                     <li>door-to-door delivery at flexible time</li>
@@ -41,10 +43,9 @@
                     <li><strong>DO</strong> not accept tips<a href="#tipping">**</a></li>
                 </ul>
             </div>
-            
 
 
-            <div id="bottombox">
+            <div>
                 <p id="tipping">**</p>
                 <p>“Would you like a smile with your General Tsao chicken with rice? That would be extra 10%. Thanks ”</p>
                 <p>-->MD strongly rejects the idea of tipping and we are promoting a non-tipping culture due to the following reasons:</p>
